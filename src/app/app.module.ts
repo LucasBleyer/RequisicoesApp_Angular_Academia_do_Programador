5bfe0287c3fba8e0f51cb2b2fdb3d17676cc561b
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-08-28 17:17:30 -0300

    departamento com o próprio routing e config no appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AuthenticationService } from './auth/services/authentication.service';
 import { PainelComponent } from './painel/painel.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DepartamentoModule } from './departamentos/departamento.module';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,9 @@ import { NavbarComponent } from './navbar/navbar.component';
     NgbModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+
+    DepartamentoModule
   ],
   providers: [AuthenticationService],
   bootstrap: [AppComponent]
--- src/app/departamentos/departamento-routing.module.ts
+++ src/app/departamentos/departamento-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepartamentoComponent } from './departamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "departamentos", component: DepartamentoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
