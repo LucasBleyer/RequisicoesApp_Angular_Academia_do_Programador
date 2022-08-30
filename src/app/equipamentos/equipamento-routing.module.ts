f472c7a6d8b57927329621f4ebd6a6fa79c5778d
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-08-30 14:23:57 -0300

    identação

--- src/app/equipamentos/equipamento-routing.module.ts
+++ src/app/equipamentos/equipamento-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EquipamentoComponent } from './equipamento.component';
 
-const routes: Routes = [{
-  path: "equipamentos", component: EquipamentoComponent
-}];
+const routes: Routes = [
+{ path: "equipamentos", component: EquipamentoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
