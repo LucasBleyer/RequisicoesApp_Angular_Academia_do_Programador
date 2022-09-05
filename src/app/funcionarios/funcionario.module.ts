87b73bc8b6013ad60f2eeff9092caef4c4a99dea
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-09-04 21:39:40 -0300

    modules, services e components de funcionario

---
+++ src/app/funcionarios/funcionario.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FuncionarioRoutingModule } from './funcionario-routing.module';
+import { FuncionarioComponent } from './funcionario.component';
+
+
+@NgModule({
+  declarations: [
+    FuncionarioComponent
+  ],
+  imports: [
+    CommonModule,
+    FuncionarioRoutingModule
+  ]
+})
+export class FuncionarioModule { }
